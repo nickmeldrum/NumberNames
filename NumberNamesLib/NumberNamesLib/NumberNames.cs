98bfbc5d28d7f6729ee55a779fbe1a8687b648ad
Nick Meldrum <nick@nickmeldrum.com>
2014-05-02 09:53:42 +0100

    1st failing test

---
+++ NumberNamesLib/NumberNamesLib/NumberNames.cs
@@ -0,0 +1,10 @@
+namespace NumberNamesLib
+{
+    public class NumberNames
+    {
+        public string Format(int input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ NumberNamesLib/NumberNamesTest/NumberNamesFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NumberNamesLib;
+
+namespace NumberNamesTest {
+    [TestFixture]
+    public class NumberNamesFixture {
+        [Test]
+        public void NumberNames_When_0IsEntered_Then_ZeroIsReturned() {
+            // arrange
+            var numberNames = new NumberNames();
+
+            // act
+            var result = numberNames.Format(0);
+
+            // assert
+            Assert.That(result, Is.EqualTo("zero"));
+        }
+    }
+}
