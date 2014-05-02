3cf2da48427d80d8c4565a8ed87441dee14c3f57
Nick Meldrum <nick@nickmeldrum.com>
2014-05-02 10:14:48 +0100

    created failing test data for hundreds

--- NumberNamesLib/NumberNamesTest/NumberNamesFixture.cs
+++ NumberNamesLib/NumberNamesTest/NumberNamesFixture.cs
@@ -16,6 +16,14 @@ namespace NumberNamesTest {
         [TestCase(42, "forty two")]
         [TestCase(77, "seventy seven")]
         [TestCase(99, "ninety nine")]
+        [TestCase(100, "one hundred")]
+        [TestCase(101, "one hundred and one")]
+        [TestCase(121, "one hundred and twenty one")]
+        [TestCase(299, "two hundred and ninety nine")]
+        [TestCase(500, "five hundred")]
+        [TestCase(911, "nine hundred and eleven")]
+        [TestCase(999, "nine hundred and ninety nine")]
+        //[TestCase(, "")]
         public void NumberNames_When_TestDataIntegerIsEntered_Then_TestDataExpectedOutputIsReturned(int input, string expectedOutput)
         {
             // arrange
