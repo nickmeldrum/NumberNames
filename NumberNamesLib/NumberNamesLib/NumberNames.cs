bfd967af8ef39546426430b6a12592c90a168a25
Nick Meldrum <nick@nickmeldrum.com>
2014-05-02 10:02:17 +0100

    got second case for 1 passing

--- NumberNamesLib/NumberNamesLib/NumberNames.cs
+++ NumberNamesLib/NumberNamesLib/NumberNames.cs
@@ -6,7 +6,9 @@ namespace NumberNamesLib
         {
             if (input == 0)
                 return "zero";
-            throw new System.NotImplementedException();
+
+            // format 1st "3 digit" set
+            return (new [] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"})[input];
         }
     }
 }
