31c6b8160a6e5477423490fc69d5e933e5e2774f
Nick Meldrum <nick@nickmeldrum.com>
2014-05-02 09:56:04 +0100

    refactored tests

--- NumberNamesLib/NumberNamesTest/NumberNamesFixture.cs
+++ NumberNamesLib/NumberNamesTest/NumberNamesFixture.cs
@@ -5,15 +5,21 @@ namespace NumberNamesTest {
     [TestFixture]
     public class NumberNamesFixture {
         [Test]
-        public void NumberNames_When_0IsEntered_Then_ZeroIsReturned() {
+        public void NumberNames_When_0IsEntered_Then_ZeroIsReturned()
+        {
+            TestNumber(0, "zero");
+        }
+
+        private void TestNumber(int input, string expectedOutput)
+        {
             // arrange
             var numberNames = new NumberNames();
 
             // act
-            var result = numberNames.Format(0);
+            var result = numberNames.Format(input);
 
             // assert
-            Assert.That(result, Is.EqualTo("zero"));
+            Assert.That(result, Is.EqualTo(expectedOutput));
         }
     }
 }
