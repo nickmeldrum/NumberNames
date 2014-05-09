1f713f480bbb225f2666a40ba3494142bde84804
Nick Meldrum <nick@nickmeldrum.com>
2014-05-09 08:49:26 +0100

    split out recombination formatter logic and tests around each

--- NumberNamesLib/Formatters/RecombinationFormatter.cs
+++ NumberNamesLib/Formatters/RecombinationFormatter.cs
@@ -1,11 +1,51 @@
-using System;
+using System.Globalization;
 
 namespace NumberNamesLib.Formatters {
     public class RecombinationFormatter : IFormat {
+        private readonly ThreeDigitFormatter _threeDigitFormatter;
+
+        public RecombinationFormatter(ThreeDigitFormatter threeDigitFormatter)
+        {
+            _threeDigitFormatter = threeDigitFormatter;
+        }
+
         public string Format(int input)
         {
-            throw new NotImplementedException();
-            return "";
+            var output = "";
+
+            var threeDigitGroups = CreateThreeDigitGroupSplit(input);
+            for (var i = 0; i < threeDigitGroups.Length; i++)
+            {
+                var threeDigitStringRepresentation = CreateThreeDigitStringRepresentation(threeDigitGroups, i);
+                output += threeDigitStringRepresentation + (i == threeDigitGroups.Length - 1 ? " and " : "");
+            }
+            return output;
+        }
+
+        public string CreateThreeDigitStringRepresentation(int[] threeDigitGroups, int position)
+        {
+            var bigNumberNames = new[] { "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion" };
+
+            return _threeDigitFormatter.Format(threeDigitGroups[position]) + bigNumberNames[threeDigitGroups.Length - position - 1];
+        }
+
+        public int[] CreateThreeDigitGroupSplit(int input)
+        {
+            var inputAsString = input.ToString(CultureInfo.InvariantCulture);
+
+            var numberOf3DigitGroups = inputAsString.Length / 3;
+            if (inputAsString.Length % 3 > 0) numberOf3DigitGroups++;
+
+            var result = new int[numberOf3DigitGroups];
+
+            for (var i = numberOf3DigitGroups; i > 0; i--)
+            {
+                result[i - 1] = int.Parse(inputAsString.Substring((inputAsString.Length - 3 > 0 ? inputAsString.Length - 3 : 0), (inputAsString.Length < 3) ? inputAsString.Length : 3));
+                if (inputAsString.Length > 3)
+                    inputAsString = inputAsString.Substring(0, inputAsString.Length - 3);
+            }
+
+            return result;
         }
     }
 }
--- NumberNamesTest/RecombinationFormatterFixture.cs
+++ NumberNamesTest/RecombinationFormatterFixture.cs
@@ -12,10 +12,50 @@ namespace NumberNamesTest {
             var recombinationFormatter = IocContainer.Container.Resolve<RecombinationFormatter>();
 
             // act
-            var result = recombinationFormatter.Format(1001);
+            var result = recombinationFormatter.Format(1002);
 
             // assert
-            Assert.That(result, Is.EqualTo("one thousand and one"));
+            Assert.That(result, Is.StringStarting("one thousand"));
+        }
+
+        [Test]
+        public void RecombinationFormatter_When_CreatingAThreeDigitRepresentationOfPosition1Of1002_Then_ResultIsOneThousand() {
+            // arrange
+            var recombinationFormatter = IocContainer.Container.Resolve<RecombinationFormatter>();
+            var array = recombinationFormatter.CreateThreeDigitGroupSplit(1002);
+
+            // act
+            var result = recombinationFormatter.CreateThreeDigitStringRepresentation(array, 0);
+
+            // assert
+            Assert.That(result, Is.EqualTo("one thousand"));
+        }
+
+        [Test]
+        public void RecombinationFormatter_When_CreatingAThreeDigitRepresentationOfPosition2Of1002_Then_ResultIsTwo() {
+            // arrange
+            var recombinationFormatter = IocContainer.Container.Resolve<RecombinationFormatter>();
+            var array = recombinationFormatter.CreateThreeDigitGroupSplit(1002);
+
+            // act
+            var result = recombinationFormatter.CreateThreeDigitStringRepresentation(array, 1);
+
+            // assert
+            Assert.That(result, Is.EqualTo("two"));
+        }
+
+        [Test]
+        public void RecombinationFormatter_When_Splitting1002Into3Digits_Then_ResultIs2IntsOf1And2() {
+            // arrange
+            var recombinationFormatter = IocContainer.Container.Resolve<RecombinationFormatter>();
+
+            // act
+            var result = recombinationFormatter.CreateThreeDigitGroupSplit(1002);
+
+            // assert
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo(1));
+            Assert.That(result[1], Is.EqualTo(2));
         }
     }
 }
